d7e34a8620d21b68a76402825a9137af9c7e8597
sangminee <lsmlee99@gmail.com>
2023-11-15 09:59:01 +0900

    .

--- 1. Java 객체 지향 프로그래밍/workspace/xciweb01/src/main/java/mvc/fx/DispatcherServlet.java
+++ 1. Java 객체 지향 프로그래밍/workspace/xciweb01/src/main/java/mvc/fx/DispatcherServlet.java
@@ -1,7 +1,11 @@
 package mvc.fx;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,79 +14,79 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import pilot.controller.FormController;
-import pilot.controller.ProcessController;
-
-@WebServlet(
-   urlPatterns = {"/pilot/*"},
-   loadOnStartup = 10)
+@WebServlet(urlPatterns = { "/pilot/*" }, loadOnStartup = 10)
 public class DispatcherServlet extends HttpServlet {
-   
-	/*
-   private Map<String, AbstractController> controllerMap = new HashMap<>();
-   @Override
-   public void init() throws ServletException {
-      
-      Properties prop = new Properties();
-      
-      try {
-         prop.load(new FileInputStream(this.getClass().getResource("dispatcher-servlet.properties").getPath()));
-         for(Object oKey : prop.keySet()) {
-            String key = ((String)oKey).trim();
-            Class<?> className = null;
-            try {
-               className = Class.forName(prop.getProperty(key).trim());
-               controllerMap.put(key, (AbstractController) className.getConstructor().newInstance());
-               System.out.println("🧡 loaded : " + className + " 🧡");
-            } catch (Exception e) {
-               e.printStackTrace();
-               System.out.println("💔 error : " + className + " 💔");
-            }
-         }
-      } catch (Exception e) {
-         e.printStackTrace();
-      }
-   }*/
-   
-   @Override
-   protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	private Map<String, AbstractController> controllerMap = new HashMap<>();
+
+	@Override
+	public void init() throws ServletException {
+
+		Properties prop = new Properties();
+		try {
+			System.out.println("  dfddfasfasdfsd"+new FileInputStream(this.getClass().getResource("dispatcher-servlet.properties").getPath()));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			prop.load(new FileInputStream(this.getClass().getResource("dispatcher-servlet.properties").getPath()));
+			for (Object oKey : prop.keySet()) {
+				String key = ((String) oKey).trim(); // 공백처리
+				Class<?> className = null;
+				try {
+					className = Class.forName(prop.getProperty(key).trim()); // 공백처리 
+					controllerMap.put(key, (AbstractController) className.getConstructor().newInstance());
+					System.out.println("🧡 loaded : " + className + " 🧡");
+				} catch (Exception e) {
+					e.printStackTrace();
+					System.out.println("💔 error : " + className + " 💔");
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String requestURI = request.getRequestURI();
 		System.out.println(requestURI);
 		String contextPath = request.getContextPath();
 		System.out.println(contextPath);
 		String action = requestURI.substring(contextPath.length());
-		
-      
-      AbstractController controller = null;
-      ModelAndView mav = null;
-      if(action.equals("/pilot/form")) {
-    	  controller = new FormController();
-    	  mav = controller.handleRequestInternal(request, response);
-      }else if(action.equals("/pilot/process")) {
-    	  controller = new ProcessController();
-    	  mav = controller.handleRequestInternal(request, response);
-      }
-      /*
-      AbstractController controller = controllerMap.get(action);
-      ModelAndView mav = controller.handleRquestInternal(request, response);
-      */
-      if (mav != null) {
-         
-         String viewName = mav.getViewName();
-         if (viewName.startsWith("redirect:")) {
-            response.sendRedirect(viewName.substring(9));
-         } else {
-            Map<String, Object> model = mav.getModel();
-            for(String key : model.keySet()) {
-               request.setAttribute(key, model.get(key));
-            }            
-            RequestDispatcher dispatcher = request.getRequestDispatcher(viewName);
-            dispatcher.forward(request, response);
-         }
-      } else {
-         System.out.println("RequestDispatcher에서 길을 잃었다네~");
-      }
 
-   }
-   
+//		AbstractController controller = null;
+//		ModelAndView mav = null;
+//		if (action.equals("/pilot/form")) {
+//			controller = new FormController();
+//			mav = controller.handleRequestInternal(request, response);
+//		} else if (action.equals("/pilot/process")) {
+//			controller = new ProcessController();
+//			mav = controller.handleRequestInternal(request, response);
+//		}
+		
+		AbstractController controller = controllerMap.get(action); 
+		ModelAndView mav =controller.handleRequestInternal(request, response);
+		
+		if (mav != null) {
+
+			String viewName = mav.getViewName();
+			if (viewName.startsWith("redirect:")) {
+				response.sendRedirect(viewName.substring(9));
+			} else {
+				Map<String, Object> model = mav.getModel();
+				for (String key : model.keySet()) {
+					request.setAttribute(key, model.get(key));
+				}
+				RequestDispatcher dispatcher = request.getRequestDispatcher(viewName);
+				dispatcher.forward(request, response);
+			}
+		} else {
+			System.out.println("RequestDispatcher에서 길을 잃었다네~");
+		}
+
+	}
+
 }
